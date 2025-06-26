c09a8c211dbf25a08739a667e4895ef5b8be2018
AkankshaDhiman <akankshadhiman2014@gmail.com>
2025-06-26 11:22:39 +0530

    Initial comment JDBC

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,68 @@
+package com.example;
+import org.postgresql.*;
+import javax.xml.stream.events.Comment;
+import java.sql.*;
+import java.sql.DriverManager;
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/university";
+        String user = "postgres";
+        String password = "Akshi2004@";
+        Connection conn = DriverManager.getConnection(url,user,password);
+        Statement stmt = conn.createStatement();
+
+        Scanner in = new Scanner(System.in);
+
+
+        String insertQuery= "INSERT INTO students(student_id,first_name,last_name,major,email,course_id) VALUES(?,?,?,?,?,?)";
+        try(PreparedStatement pstmt = conn.prepareStatement(insertQuery)){
+            System.out.print("Enter student ID : ");
+            int id = in.nextInt();
+            pstmt.setInt(1,id);
+            System.out.print("Enter student First_name : ");
+            String first_name = in.next();
+            pstmt.setString(2,first_name);
+            System.out.print("Enter student Last_name : ");
+            String last_name = in.next();
+            pstmt.setString(3,last_name);
+            System.out.print("Enter major : ");
+            String major = in.next();
+            pstmt.setString(4,major);
+            System.out.print("Enter student Email : ");
+            String email = in.next();
+            pstmt.setString(5,email);
+            System.out.print("Enter course_id : ");
+            int course_id = in.nextInt();
+            pstmt.setInt(6,course_id);
+            int check =pstmt.executeUpdate();
+            if(check>0){
+                System.out.println("Student Added Successfully");
+            }else {
+                System.out.println("Insertion Failed");
+            }
+        }
+        catch(SQLException e){
+            System.out.println(e);
+        }
+
+        Statement stmt2 = conn.createStatement();
+        String query = "SELECT * FROM students";
+        ResultSet rs = stmt2.executeQuery(query);
+        while(rs.next()){
+            System.out.print("\n"+rs.getString("student_id"));
+            System.out.print("\t"+rs.getString("first_name"));
+            System.out.print("\t"+rs.getString("last_name"));
+            System.out.print("\t"+rs.getString("major"));
+            System.out.print("\t"+rs.getString("email"));
+            System.out.print("\t"+rs.getString("course_id"));
+        }
+        in.close();
+    }
+}
